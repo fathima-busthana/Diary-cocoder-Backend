58eb91a48a420e9a721bb904fb160ec6dfca268a
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-02-22 19:15:03 +0530

    added jwt sign in token

--- controller/authController.js
+++ controller/authController.js
@@ -1,21 +1,31 @@
 const users = require("../model/user");
 const appError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+const jwt = require("jsonwebtoken");
+
+//function to crate jwt token for
+const createToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 //signup function
 exports.signUp = catchAsync(async (req, res, next) => {
-  const { username, email, password, confirmPassword } = req.body;
-  if (password !== confirmPassword) {
-    return next(new appError("password mismatch", 401));
-  }
+  const { username, email, password, passwordConfirm } = req.body;
+
   const response = await users.create({
     username: username,
     email: email,
     password: password,
+    passwordConfirm: passwordConfirm,
   });
+  //creating jwt token
+  const token = createToken(response._id);
   res.status(200).json({
     ok: true,
     data: response,
+    token,
   });
   //   next();
 });
